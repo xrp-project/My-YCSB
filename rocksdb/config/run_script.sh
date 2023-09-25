16dd5ee9e1ce8a2730d1e0813d671024ac6ac19d
jcarin <jcarin@localhost>
2023-09-25 17:58:45 -0600

    add second command

--- rocksdb/config/run_script.sh
+++ rocksdb/config/run_script.sh
@@ -1,12 +1,31 @@
+TEST="local_indexes_unpinned.json"
+
 nohup python3 bench_rocksdb.py \
-    --results-file=/mydata/jeremy-home/rocksdb_zipf_results.json \
-    --cpu 2 \
-    --db-path=/mydata/database \
-    --bench-path=/mydata/jeremy-home/My-YCSB/build/run_rocksdb \
-    --config-dir=/mydata/jeremy-home/My-YCSB/rocksdb/config \
+    --results-file=/mydata/rocksdb-results/"$TEST" \
+    --cpu 3 \
+    --db-path=/mydata/database/rocksdb \
+    --bench-path=/mydata/My-YCSB/build/run_rocksdb \
+    --config-dir=/mydata/My-YCSB/rocksdb/config \
     --temp-db-path=/mydata/datebase_temp \
     --disk-type=local \
     --threads-per-core=8 \
-    > /mydata/jeremy-home/bench_rocksdb.log \
+    > /mydata/bench_rocksdb.log \
     2>&1 & \
-tail -f /mydata/jeremy-home/bench_rocksdb.log
+tail -f /mydata/bench_rocksdb.log
+
+TEST="bpfof_indexes_pinned.json"
+nohup python3 bench_rocksdb.py \
+    --results-file=/mydata/rocksdb-results/"$TEST" \
+    --cpu 3 \
+    --db-path=/nvme/rocksdb \
+    --bench-path=/mydata/My-YCSB/build/run_rocksdb \
+    --config-dir=/mydata/My-YCSB/rocksdb/config \
+    --temp-db-path=/nvme/rocksdb_temp_2 \
+    --disk-type=nvmeof_tcp \
+    --threads-per-core=8 \
+    > /mydata/bench_rocksdb.log \
+    2>&1 & \
+tail -f /mydata/bench_rocksdb.log
+
+
+# DONT FORGET TO CHANGE INDEXES PINNED
